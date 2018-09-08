f9d010286015cb1d2ce47d4a7c68aab6dcb840be
yanxichen <yanxi.yc@gmail.com>
2018-09-08 02:28:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# connect four
+
+Play against AI in this classic connect four game. Built with Python, PyGame and NumPy, achieved through 
+Minimax algorithm and Alpha-Beta pruning.
+
+![](https://i.imgur.com/OLvYO97.gif)
+
+
+## Installation
+
+Use `$ pip install .` to install the package and its dependencies. Run `connect4` to start the game.
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+with open("requirements.txt") as f:
+    dependencies = f.read().splitlines()
+
+setup(
+    name='connectfour',
+    version='1.0.0',
+    description='An implementation of the classic Connect Four game',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown',
+    # url='https://github.com/yanxichen/instagrow',
+    author='Yanxi Chen',
+    # author_email='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='connectfour',
+    packages=['connect4'],
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'connect4=connect4.connect4',
+        ],
+    },
+)
